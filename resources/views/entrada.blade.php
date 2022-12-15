6dd56db7a430786aef9f138f5ee5476ad3cef00b
Shaiane Pastore <shaypastore@hotmail.com>
2022-12-15 17:00:24 -0300

    ajustes

--- resources/views/entrada.blade.php
+++ resources/views/entrada.blade.php
@@ -2,17 +2,23 @@
 @section('titulo', 'Página inicial')
 @section('corpo')
 
-<div class="px-4 pt-5 my-5 text-center border-bottom">
-    <h1 class="display-4 fw-bold">SOS Pet</h1>
-    <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4"> [texto descritivo]</p>
-    </div>
-    <div class="overflow-hidden" style="max-height: 30vh;">
-        <div class="container px-5">
-            <img src="https://melhoramigo.dog/wp-content/uploads/2016/02/ongs-que-ajudam-animais-de-rua.jpg" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
-        </div>
-    </div>
-</div>
+
+<header class="masthead px-4 pt-5 my-5 text-center border-bottom" style=" background: url('https://melhoramigo.dog/wp-content/uploads/2016/02/ongs-que-ajudam-animais-de-rua.jpg') center no-repeat; background-size: cover">
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="site-heading">
+                            <h1>SOS Pet</h1>
+                            <span class="subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
 
 <div class="container px-4 px-lg-5">
     <nav class="navbar navbar-light bg-light">
